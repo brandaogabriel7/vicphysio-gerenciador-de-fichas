01121773707b9f9461dd23dd6d14d496447a7bba
Gabriel Brandão <29805002+brandaogabriel7@users.noreply.github.com>
2024-12-18 21:57:31 -0300

    feat: implementa campo comum das fichas

--- src/domain/cliente/entity/ficha.spec.ts
+++ src/domain/cliente/entity/ficha.spec.ts
@@ -1,8 +1,10 @@
 import Cliente from './cliente';
 import Ficha from './ficha';
 import { v4 as uuid } from 'uuid';
-import { SexoEnum } from './sexo';
+import { SexoEnum } from './enum/sexo';
 import DataGenerica from '../value-object/data-generica';
+import { HistoriaPatologicaPregressaEnum } from './enum/historia-patologica-pregressa';
+import { QualidadeAlimentacaoEnum } from './enum/qualidade-alimentacao';
 
 describe('Ficha tests', () => {
   it('deve falhar quando id está vazio', () => {
@@ -28,30 +30,74 @@ describe('Ficha tests', () => {
     expect(ficha).toBeInstanceOf(Ficha);
     expect(ficha.id).toBe(id);
     expect(ficha.cliente).toBe(cliente);
-    expect(ficha.data).toBeUndefined();
-
-    const dataFicha = new DataGenerica(new Date());
-    const ficha2 = new Ficha(id, cliente, dataFicha);
-
-    expect(ficha2).toBeInstanceOf(Ficha);
-    expect(ficha2.id).toBe(id);
-    expect(ficha2.cliente).toBe(cliente);
-    expect(ficha2.data).toBe(dataFicha);
   });
 
-  it('deve alterar a data da ficha', () => {
-    const cliente = new Cliente(
-      uuid(),
-      'Brands',
-      '12345678',
-      SexoEnum.MASCULINO
+  describe('campos da ficha', () => {
+    let ficha: Ficha;
+    beforeEach(() => {
+      const cliente = new Cliente(
+        uuid(),
+        'Brands',
+        '12345678',
+        SexoEnum.MASCULINO
+      );
+      ficha = new Ficha(uuid(), cliente);
+    });
+
+    it('deve alterar a data da ficha', () => {
+      const dataFicha = new DataGenerica(new Date());
+      ficha.alterarDataFicha(dataFicha);
+
+      expect(ficha.data).toBe(dataFicha);
+    });
+
+    it.each([
+      ['Dor nas costas'],
+      ['Dor nos joelhos'],
+      ['Dor nos pés'],
+      ['Dor nos ombros'],
+    ])(
+      'deve alterar historia da molestia atual para "%s"',
+      (historiaMolestiaAtual) => {
+        ficha.alterarHistoriaMolestiaAtual(historiaMolestiaAtual);
+
+        expect(ficha.historiaMolestiaAtual).toBe(historiaMolestiaAtual);
+      }
     );
-    const id = uuid();
-    const ficha = new Ficha(id, cliente);
 
-    const dataFicha = new DataGenerica(new Date());
-    ficha.alterarDataFicha(dataFicha);
+    it.each([
+      ...Object.values(HistoriaPatologicaPregressaEnum).map((hpp) => [hpp]),
+    ])('deve alterar historia patologica pregressa para "%s"', (hma) => {
+      ficha.alterarHistoriaPatologiaPregressa(hma);
 
-    expect(ficha.data).toBe(dataFicha);
+      expect(ficha.historiaPatologiaPregressa).toBe(hma);
+    });
+
+    it.each([...Object.values(QualidadeAlimentacaoEnum).map((qa) => [qa])])(
+      'deve alterar qualidade alimentacao para "%s"',
+      (qualidadeAlimentação) => {
+        ficha.alterarQualidadeAlimentacao(qualidadeAlimentação);
+
+        expect(ficha.qualidadeAlimentacao).toBe(qualidadeAlimentação);
+      }
+    );
+
+    it.each([['Remédio 1'], ['Remédio 2'], ['Remédio 3'], ['Remédio 4']])(
+      'deve alterar campo de medicações para "%s"',
+      (medicacoes) => {
+        ficha.alterarMedicacoes(medicacoes);
+
+        expect(ficha.medicacoes).toBe(medicacoes);
+      }
+    );
+
+    it.each([['Observações 1'], ['Observações 2'], ['Observações 3']])(
+      'deve alterar campo de observações para "%s"',
+      (observacoes) => {
+        ficha.alterarObservacoes(observacoes);
+
+        expect(ficha.observacoes).toBe(observacoes);
+      }
+    );
   });
 });
--- src/domain/cliente/entity/ficha.ts
+++ src/domain/cliente/entity/ficha.ts
@@ -1,19 +1,26 @@
 import DataGenerica from '../value-object/data-generica';
 import Cliente from './cliente';
+import { HistoriaPatologicaPregressa } from './enum/historia-patologica-pregressa';
+import { QualidadeAlimentacao } from './enum/qualidade-alimentacao';
 
 export default class Ficha {
   private _id: string;
   private _cliente: Cliente;
   private _data?: DataGenerica;
 
-  constructor(id: string, cliente: Cliente, data?: DataGenerica) {
+  private _historiaMolestiaAtual?: string;
+  private _historiaPatologiaPregressa?: HistoriaPatologicaPregressa;
+  private _qualidadeAlimentacao?: QualidadeAlimentacao;
+  private _medicacoes?: string;
+  private _observacoes?: string;
+
+  constructor(id: string, cliente: Cliente) {
     if (!id) {
       throw new Error('id é obrigatório');
     }
 
     this._id = id;
     this._cliente = cliente;
-    this._data = data;
   }
 
   get id(): string {
@@ -28,7 +35,51 @@ export default class Ficha {
     return this._data;
   }
 
+  get historiaMolestiaAtual(): string | undefined {
+    return this._historiaMolestiaAtual;
+  }
+
+  get historiaPatologiaPregressa(): HistoriaPatologicaPregressa | undefined {
+    return this._historiaPatologiaPregressa;
+  }
+
+  get qualidadeAlimentacao(): QualidadeAlimentacao | undefined {
+    return this._qualidadeAlimentacao;
+  }
+
+  get medicacoes(): string | undefined {
+    return this._medicacoes;
+  }
+
+  get observacoes(): string | undefined {
+    return this._observacoes;
+  }
+
   alterarDataFicha(data: DataGenerica): void {
     this._data = data;
+  }
+
+  alterarHistoriaMolestiaAtual(historiaMolestiaAtual: string): void {
+    this._historiaMolestiaAtual = historiaMolestiaAtual;
+  }
+
+  alterarHistoriaPatologiaPregressa(
+    historiaPatologiaPregressa: HistoriaPatologicaPregressa
+  ): void {
+    this._historiaPatologiaPregressa = historiaPatologiaPregressa;
+  }
+
+  alterarQualidadeAlimentacao(
+    qualidadeAlimentacao: QualidadeAlimentacao
+  ): void {
+    this._qualidadeAlimentacao = qualidadeAlimentacao;
+  }
+
+  alterarMedicacoes(medicacoes: string): void {
+    this._medicacoes = medicacoes;
+  }
+
+  alterarObservacoes(observacoes: string): void {
+    this._observacoes = observacoes;
   }
 }
